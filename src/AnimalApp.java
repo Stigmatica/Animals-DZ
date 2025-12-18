bd78d8c8d23f2a24bb0f3c5dbdcf7e25756234a3
Alexander <legendary510@gmail.com>
2025-12-18 17:33:45 +0300

    #HOMEWORK-№1

---
+++ src/AnimalApp.java
@@ -0,0 +1,115 @@
+import animals.Animal;
+import animals.Color;
+import factory.AnimalFactory;
+import factory.AnimalType;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
+        AnimalFactory animalFactory = new AnimalFactory();
+        Scanner scanner = new Scanner(System.in);
+        Command currentCommand = null;
+
+        while (currentCommand !=Command.EXIT) {
+            currentCommand = getCommand(scanner);
+
+            if (currentCommand == Command.LIST) {
+                if (animals.isEmpty()) {
+                    System.out.println("Список пуст");
+                }
+                for (Animal animal : animals) {
+                    System.out.println(animal);
+                }
+            } else if (currentCommand == Command.ADD) {
+
+                AnimalType animalType = null;
+                while (animalType == null) {
+                    System.out.print("Какое животное (cat/dog/duck)? ");
+                    String animalTypeInput = scanner.next().toUpperCase();
+
+                    try {
+                        animalType = AnimalType.valueOf(animalTypeInput);
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Неверный тип животного. Попробуйте снова.");
+                    }
+                }
+
+                // Имя
+                System.out.print("Введите имя: ");
+                String name = scanner.next().trim();
+
+                int age = 0;
+                while (true) {
+                    try {
+                        System.out.print("Введите возраст: ");
+                        age = Integer.parseInt(scanner.next());
+
+                        if (age >= 0) {
+                            break;
+                        } else {
+                            System.out.println("Возраст не может быть меньше 0. Повторите ввод");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Возраст должен быть в числовом формате.");
+                    }
+                }
+
+                // Вес
+                int weight = 0;
+                while (true) {
+                    try {
+                        System.out.print("Введите вес: ");
+                        weight = Integer.parseInt(scanner.next());
+
+                        if (weight >= 0) {
+                            break;
+                        } else {
+                            System.out.println("Вес не может быть меньше 0. Повторите ввод");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Вес должен быть в числовом формате.");
+                    }
+                }
+
+                // Цвет
+                Color color = Color.UNDEFINED;
+                System.out.print("Введите цвет (WHITE/BLACK или другой): ");
+                String colorInput = scanner.next().toUpperCase();
+                try {
+                    color = Color.valueOf(colorInput);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Цвет не распознан, будет использован цвет по умолчанию");
+                }
+                Animal animal = animalFactory.create(animalType);
+
+                animal.setName(name);
+                animal.setAge(age);
+                animal.setWight(weight);
+                animal.setColor(color);
+
+                animals.add(animal);
+                animal.say();
+
+                System.out.println("Животное успешно добавлено!\n");
+            }
+        }
+        scanner.close();
+    }
+        private static Command getCommand(Scanner scanner) {
+        String commandInput = null;
+        while (Command.doesNotContain(commandInput)) {
+            if (commandInput != null) {
+                System.out.println("Введена неверная команда");
+            }
+            System.out.printf("Введите одну из команд (%s): ", String.join("/", Command.NAMES));
+            commandInput = scanner.next();
+        }
+        return  Command.fromString(commandInput);
+    }
+
+}
---
+++ src/animals/Animal.java
@@ -0,0 +1,66 @@
+package animals;
+
+public class Animal {
+    private String name;
+    private int age;
+    private int wight;
+    private Color color = Color.UNDEFINED;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWight() {
+        return wight;}
+
+    public void setWight(int wight) {
+        this.wight = wight;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void say() {
+        System.out.println("Я говорю");
+    }
+    public void go() {
+        System.out.println("Я иду");
+    }
+    public void drink() {
+        System.out.println("Я пью");
+    }
+    public void eat() {
+        System.out.println("Я ем");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Привет! Меня зовут %s, мне %d %s, я вешу %d кг, мой цвет - %s", name, age, getAgeSuffix(), wight, color.getValue());
+    }
+
+    private String getAgeSuffix() {
+        int reminder10 = age % 10;
+        int reminder100 = age % 100;
+        if (reminder10 == 1 && reminder100 != 11) {
+            return "год";
+        }
+        if (reminder10 >= 2 && reminder10 <= 4 && reminder100 != 12 && reminder100 != 13 && reminder100 != 14) {
+            return "года";
+        }
+        return  "лет";
+    }
+}
